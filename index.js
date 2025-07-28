b4303ab27d99552c402f806fae8e8d2d07cf1728
CopyypoC <william.wang650@gmail.com>
2025-07-27 19:51:13 -0700

    feat: rewrite route handlers with Express

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
-import http from "node:http";
-import fs, { read } from "node:fs";
+import express from "express";
 import path from "node:path";
 
-const port = 8080;
+const app = express();
+const PORT = 8080;
 const __dirname = import.meta.dirname;
-const notFoundPath = path.join(__dirname, "404.html");
-const routes = {
-  "/": path.join(__dirname, "index.html"),
-  "/about": path.join(__dirname, "about.html"),
-  "/contact-me": path.join(__dirname, "contact-me.html"),
-};
 
-const server = http.createServer((req, res) => {
-  const filePath = routes[req.url];
-  if (filePath) {
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        return res.end("500 Internal Server Error");
-      }
+function handleRoute(res, path) {
+  res.sendFile(path, (err) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+  });
+}
 
-      res.writeHead(200, { "Content-Type": "text/html" });
-      return res.end(data);
-    });
-  } else {
-    fs.readFile(notFoundPath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        return res.end("500 Internal Server Error");
-      }
-
-      res.writeHead(404, { "Content-Type": "text/html" });
-      return res.end(data);
-    });
-  }
+app.get("/", (req, res) => {
+  handleRoute(res, path.join(__dirname, "index.html"));
 });
 
-server.listen(port, (err) => {
+app.get("/about", (req, res) => {
+  handleRoute(res, path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  handleRoute(res, path.join(__dirname, "contact-me.html"));
+});
+
+// 404 Error Handler
+app.use((req, res) => {
+  handleRoute(res, path.join(__dirname, "404.html"));
+});
+
+app.listen(PORT, (err) => {
   if (err) {
     console.log("Server Error: ", err);
   } else {
-    console.log("Server listening on port " + port);
+    console.log("Server listening on port " + PORT);
   }
 });
